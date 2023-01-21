14c1737687d56c0c7bd574419019b1c611a0a944
shreekarSS <shreekara.ss@gmail.com>
2023-01-21 17:35:14 -0500

    LinkedList problems and solutions
    Signed-off-by: shreekarSS <shreekara.ss@gmail.com>

---
+++ LinkedList/21. Merge Two Sorted Lists.py
@@ -0,0 +1,35 @@
+# Definition for singly-linked list.
+from typing import Optional
+
+
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+class Solution:
+    def mergeTwoLists(self, list1: Optional[ListNode], list2: Optional[ListNode]) -> Optional[ListNode]:
+        """
+        Input: list1 = [1,2,4], list2 = [1,3,4]
+        Output: [1,1,2,3,4,4]
+        """
+        curr = dummy = ListNode()
+
+        while list1 and list2:
+
+            if list1.val < list2.val:
+                curr.next = list1
+                list1 = list1.next
+            else:
+                curr.next = list2
+                list2 = list2.next
+
+            curr = curr.next
+
+
+        curr.next = list1 if list1 else list2
+
+        return dummy.next
+
+
+
+
