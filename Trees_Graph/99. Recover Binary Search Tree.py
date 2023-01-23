2c985ae9a679573b0ecaea4efaed3eed0ba19723
shreekarSS <shreekara.ss@gmail.com>
2023-01-23 17:24:52 -0500

    99. Recover Binary Search Tree
    Signed-off-by: shreekarSS <shreekara.ss@gmail.com>

---
+++ Trees_Graph/99. Recover Binary Search Tree.py
@@ -0,0 +1,31 @@
+# Definition for a binary tree node.
+from typing import Optional
+
+
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+class Solution:
+    def recoverTree(self, root: Optional[TreeNode]) -> None:
+        """
+        Do not return anything, modify root in-place instead.
+        """
+
+        self.temp = []
+        def dfs(root):
+            if not root:
+                return
+            dfs(root.left)
+            self.temp.append(root)
+            dfs(root.right)
+
+        dfs(root)
+
+        sort = sorted(node.val for node in self.temp)
+
+
+        for i in range(len(sort)):
+            print(sort[i])
+            self.temp[i].val = sort[i]
